e19d4dddcc4a85ca83701bc0f2ca4e60eedb1260
Reza Kajbaf <kajbafr@gmail.com>
2017-03-19 12:22:53 -0400

    Update app.js

--- Slack/express_example/app.js
+++ Slack/express_example/app.js
@@ -29,6 +29,7 @@ app.use('/users', users);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
+  Console.log("An error occured!!!");
   err.status = 404;
   next(err);
 });
